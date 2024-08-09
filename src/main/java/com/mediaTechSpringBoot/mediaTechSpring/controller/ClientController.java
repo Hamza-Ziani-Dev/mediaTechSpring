dfd122a86c1c21bd7c564bc700bb965b817c3403
Hamza-Ziani-Dev <hamzaziani.dev@gmail.com>
2024-08-09 15:48:16 +0100

    'add response entity'

--- src/main/java/com/mediaTechSpringBoot/mediaTechSpring/controller/ClientController.java
+++ src/main/java/com/mediaTechSpringBoot/mediaTechSpring/controller/ClientController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -37,41 +39,47 @@ public class ClientController {
 
    
     @GetMapping("")
-    public List<ClientResponseDto> getAllClients() {
-        return clientService.findAll();
+    public ResponseEntity<List<ClientResponseDto>>  getAllClients() {
+        return new ResponseEntity<List<ClientResponseDto>>(clientService.findAll(), HttpStatus.OK);
     }
 
 
 
 
     @PostMapping("")
-    public ClientResponseDto saveClient(@RequestBody() ClientRequestDto ClientRequestDto) {
-        return clientService.saveClient(ClientRequestDto);
+    public ResponseEntity<ClientResponseDto> saveClient(@RequestBody() ClientRequestDto ClientRequestDto) {
+        ClientResponseDto clientResponseDto = clientService.saveClient(ClientRequestDto);
+        return new ResponseEntity<ClientResponseDto>(clientResponseDto, HttpStatus.CREATED);
+
     }
 
     // get Client By Id :  http://localhost:8080/clients/id/1
     @GetMapping("/id/{id}")
-    public ClientResponseDto getClientById(@PathVariable("id") Integer id) {
-        return clientService.getClientById(id);
+    public ResponseEntity<ClientResponseDto> getClientById(@PathVariable("id") Integer id) {
+        ClientResponseDto clientResponseDto = clientService.getClientById(id);
+        return ResponseEntity.ok(clientResponseDto);
     }
 
     // Get Client By Nom :  http://localhost:8080/clients/nom/ahmed
     @GetMapping("/nom/{nom}")
-    public ClientResponseDto getClientByNom(@PathVariable("nom") String nom) {
-        return clientService.getClientByNom(nom);
+    public ResponseEntity<ClientResponseDto> getClientByNom(@PathVariable("nom") String nom) {
+        ClientResponseDto clientResponseDto = clientService.getClientByNom(nom);
+        return ResponseEntity.ok(clientResponseDto);
     }
 
     // Update Client By Id :  http://localhost:8080/clients/id/1
     @PutMapping("/id/{id}")
-    public ClientResponseDto updateClient(@RequestBody() ClientRequestDto clientRequestDto, @PathVariable("id") Integer id) {
-        return clientService.updateClient(clientRequestDto, id);
+    public ResponseEntity<ClientResponseDto> updateClient(@RequestBody() ClientRequestDto clientRequestDto, @PathVariable("id") Integer id) {
+        ClientResponseDto clientResponseDto = clientService.updateClient(clientRequestDto, id);
+        return ResponseEntity.accepted().body(clientResponseDto);
     }
 
 
     // Delete Client By Id :  http://localhost:8080/clients/id/1
     @DeleteMapping("/id/{id}")
-    public void deleteClient(@PathVariable("id") Integer id) {
+    public ResponseEntity<?> deleteClient(@PathVariable("id") Integer id) {
         clientService.deleteClient(id);
+        return ResponseEntity.noContent().build();
     }
     
     
